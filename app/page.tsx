678220f689cf946a45d941f22ddb6b6123cf5c59
k786 <tausheefk786@gmail.com>
2025-09-11 01:44:05 +0530

    02 database

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import { db } from "@/lib/db";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
   return (
    <div className="flex flex-col items-center justify-center h-screen mask-b-from-gray-100">
     <button>
